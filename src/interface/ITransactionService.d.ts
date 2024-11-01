ca271b59650223670dffe3399f30d4fc909b1c8d
Gustavo Tatarem <gus.tatarem@gmail.com>
2024-11-01 12:25:15 -0300

    fix: code cleanup

--- src/interface/ITransactionService.d.ts
+++ src/interface/ITransactionService.d.ts
@@ -3,10 +3,12 @@ export default interface ITransactionService {
     accountId: string,
     amount: number
   ): { destination: { id: string; balance: number } };
+
   withdrawFromAccount(
     accountId: string,
     amount: number
   ): { origin: { id: string; balance: number } };
+
   transfer(
     originId: string,
     destinationId: string,
