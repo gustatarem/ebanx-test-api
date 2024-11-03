e904b38e9e6e6043e6fe6b5ec039febb6bf2ef1d
Gustavo Tatarem <gus.tatarem@gmail.com>
2024-11-03 13:38:37 -0300

    feature: add tests for non existent accounts

--- tests/TransactionController.test.ts
+++ tests/TransactionController.test.ts
@@ -63,6 +63,27 @@ describe("TransactionController", () => {
     });
   });
 
+  it("should not withdraw from a non existent account", async () => {
+    const account = { id: "100", balance: 10 };
+    transactionService.withdrawFromAccount.mockImplementation(() => {
+      throw new Error("Account not found.");
+    });
+
+    request.body = { type: "withdraw", origin: "999", amount: 5 };
+
+    await transactionController.handleEvent(
+      request as FastifyRequest,
+      reply as FastifyReply
+    );
+
+    expect(transactionService.withdrawFromAccount).toHaveBeenCalledWith(
+      "999",
+      5
+    );
+    expect(reply.status).toHaveBeenCalledWith(404);
+    expect(reply.send).toHaveBeenCalledWith("0");
+  });
+
   it("should transfer from an account to another", async () => {
     const originAccount = { id: "100", balance: 50 };
 
@@ -89,6 +110,28 @@ describe("TransactionController", () => {
       origin: { id: "100", balance: 20 },
       destination: { id: "200", balance: 30 },
     });
+  });
+
+  it("should not transfer from an non existent account", async () => {
+    request.body = {
+      type: "transfer",
+      origin: "999",
+      destination: "200",
+      amount: 30,
+    };
+
+    transactionService.transfer.mockImplementation(() => {
+      throw new Error("Account not found.");
+    });
+
+    await transactionController.handleEvent(
+      request as FastifyRequest,
+      reply as FastifyReply
+    );
+
+    expect(transactionService.transfer).toHaveBeenCalledWith("999", "200", 30);
+    expect(reply.status).toHaveBeenCalledWith(404);
+    expect(reply.send).toHaveBeenCalledWith("0");
   });
 
   test("should return 404 for invalid event type", async () => {
